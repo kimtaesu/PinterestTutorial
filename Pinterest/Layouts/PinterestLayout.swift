a38b332e738368deb0d0e6392e9a033029a57163
kimtaesu <kimtaesoo188@gmail.com>
2019-07-27 23:03:06 +0900

    first commit

---
+++ Pinterest/Layouts/PinterestLayout.swift
@@ -0,0 +1,14 @@
+//
+//  PinterestLayout.swift
+//  Pinterest
+//
+//  Created by tskim on 27/07/2019.
+//  Copyright © 2019 Razeware LLC. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class PinterestLayout: UICollectionViewFlowLayout {
+  
+}
---
+++ README.md
@@ -0,0 +1,4 @@
+
+## [Pinterest Tutorial](https://www.raywenderlich.com/392-uicollectionview-custom-layout-tutorial-pinterest)
+
+![](https://koenig-media.raywenderlich.com/uploads/2015/05/layout-lifecycle.png)
